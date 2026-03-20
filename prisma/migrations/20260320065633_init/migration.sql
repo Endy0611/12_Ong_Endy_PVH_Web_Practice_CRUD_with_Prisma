8e34eeddab014f4f81e50fff33c10d37e2d63c9c
Endy0611 <endyong18@gmail.com>
2026-03-20 13:57:52 +0700

    fetch connection again

--- prisma/migrations/20260320065633_init/migration.sql
+++ prisma/migrations/20260320065633_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "students" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "class" TEXT NOT NULL,
 
     CONSTRAINT "students_pkey" PRIMARY KEY ("id")
 );
